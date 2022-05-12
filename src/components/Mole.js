ed1487f7a838056001b4a19220862f028eda23b6
Ahmad AlAmiri <a.alamiri.96@gmail.com>
2022-05-12 20:05:29 +0300

    initial commit

---
+++ src/components/Mole.js
@@ -0,0 +1,9 @@
+import img from "../images/avo1.png";
+
+export default function Mole() {
+  return (
+    <div className="mole">
+      <img className="mole-img" src={img} alt="avocado" />
+    </div>
+  );
+}
